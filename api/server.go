ed9eb59820ca49cc982d57b7aec6d03257b9a319
Bhaskar Hazarika <bhaskar.hazarika@westernunion.com>
2022-05-02 23:06:55 -0700

    Added  transfer service

--- api/server.go
+++ api/server.go
@@ -4,6 +4,8 @@ import (
 	db "gopractice/simplebank/db/sqlc"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
+	"github.com/go-playground/validator/v10"
 	_ "github.com/lib/pq"
 )
 
@@ -18,10 +20,15 @@ func NewServer(store *db.Store) *Server {
 	server := &Server{store: store}
 	router := gin.Default()
 
+	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
+		v.RegisterValidation("currency", validCurrency)
+	}
+
 	router.POST("/accounts", server.CreateAccount)
 	router.GET("/accounts/:id", server.getAccount)
 	router.GET("/accounts", server.listAccount)
 	router.GET("/entries/:id", server.getEntry)
+	router.POST("/transfer", server.CreateTranfer)
 	server.router = router
 	return server
 }
